23c9e73d594f0307848af848732914a3feedd60a
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-11 16:45:19 +1000

    commit tree f4a28124bc957c33f25828ba3edda921e2efe7b8

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) { 
+  return "This one is on me!"
+} else if (ride > 2500) {
+  return 'No can do.';
+} else {
+  return 'I will gladly take your thirty bucks.'
+}}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city === 'NYC') {
+  return 'Ok, sounds good.';
+} else return 'No go.';
 }
+/*
+function switchOnCharmFromTip(tipValue){
+switch (tipValue) {
+  case (100):
+    console.log('Thank you so much.');
+    break;
+    case (50):
+      console.log('Thank you.');
+      break;
+      case (0):
+      console.log('Bye.');
+}
+}
+*/
+function switchOnCharmFromTip(tip) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
 }
